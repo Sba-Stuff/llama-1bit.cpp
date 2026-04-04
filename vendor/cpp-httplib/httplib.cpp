d934971d0c991c56764b7f3e46e797eea7b50f69
Sba Stuff <36412543+Sba-Stuff@users.noreply.github.com>
2026-04-04 10:44:00 +0500

    Change WriteFile2 to WriteFile2 Something
    Change WriteFile2 to WriteFile2 Something as it gives compilation error.

--- vendor/cpp-httplib/httplib.cpp
+++ vendor/cpp-httplib/httplib.cpp
@@ -1434,8 +1434,8 @@ bool mmap::open(const char *path) {
   auto wpath = u8string_to_wstring(path);
   if (wpath.empty()) { return false; }
 
-  hFile_ = ::CreateFile2(wpath.c_str(), GENERIC_READ, FILE_SHARE_READ,
-                         OPEN_EXISTING, NULL);
+  hFile_ = ::CreateFileW(wpath.c_str(), GENERIC_READ, FILE_SHARE_READ,
+                       nullptr, OPEN_EXISTING, 0, nullptr);
 
   if (hFile_ == INVALID_HANDLE_VALUE) { return false; }
 
